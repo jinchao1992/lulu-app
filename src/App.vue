77905a7f950b07712daff3eeaf61e32945373498
jinchao1992 <jinchaophp@126.com>
2020-11-26 23:17:16 +0800

    :lipstick: 初始化 aside 组件并添加样式

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,4 @@
 <template>
-  <div>
-    <router-link to="/">首页</router-link>
-    |
-    <router-link to="/doc">文档</router-link>
-  </div>
-  <hr/>
   <router-view/>
 </template>
 
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,7 +2,23 @@
   <div>
     <TabNav/>
     <div class="content">
-      <aside>边栏</aside>
+      <aside>
+        <h1>菜单列表</h1>
+        <ol>
+          <li>
+            <router-link to="/doc/switch">Switch 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Button 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/diglog">Diglog 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs 组件</router-link>
+          </li>
+        </ol>
+      </aside>
       <main>主体</main>
     </div>
   </div>
@@ -19,6 +35,23 @@
   };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  aside {
+    background: lightblue;
+    width: 150px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding: 70px 16px 16px;
 
+    > h2 {
+      margin-bottom: 4px;
+    }
+
+    > ol {
+      > li {
+        padding: 4px 0;
+      }
+    }
+  }
 </style>
