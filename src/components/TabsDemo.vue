667d91aecb5fe426101e5ca4ba958932ac4c3fad
jinchao1992 <jinchaophp@126.com>
2020-12-06 22:47:52 +0800

    :art: Tabs 可以切换了

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <h1>Tabs 示例1</h1>
-    <Tabs>
-      <Tab title="导航1">
+    <Tabs :selected="defaultSelected" @update:selected=" defaultSelected = $event">
+      <Tab title="导航1" key="1">
         内容1
       </Tab>
-      <Tab title="导航2">
+      <Tab title="导航2" key="2">
         内容2
       </Tab>
     </Tabs>
@@ -15,10 +15,17 @@
 <script lang="ts">
   import Tabs from '../lib/Tabs.vue';
   import Tab from '../lib/Tab.vue';
+  import { ref } from 'vue';
 
   export default {
     name: 'TabsDemo',
-    components: { Tabs, Tab }
+    components: { Tabs, Tab },
+    setup() {
+      const defaultSelected = ref('1');
+      return {
+        defaultSelected
+      };
+    }
   };
 </script>
 
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,19 +1,30 @@
 <template>
   <div class="lulu-tabs">
     <div class="lulu-tabs-nav">
-      <div class="lulu-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+      <div
+        class="lulu-tabs-nav-item"
+        :class="{selected: selected === n.key}"
+        v-for="(n,index) in navs"
+        :key="index"
+        @click="() => select(n.key)"
+      >{{n.title}}
+      </div>
     </div>
     <div class="lulu-tabs-content">
-      <component class="lulu-tabs-content-item" v-for="(c, index) in defaults" :is="c" :key="index"/>
+      <component class="lulu-tabs-content-item" :is="current"/>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+  import { computed } from 'vue';
   import Tab from './Tab.vue';
 
   export default {
     name: 'Tabs',
+    props: {
+      selected: String
+    },
     setup(props, context) {
       const defaults = context.slots.default();
       defaults.forEach(node => {
@@ -21,12 +32,26 @@
           throw new Error('标签名称不对，请更换');
         }
       });
-      const titles = defaults.map(node => {
-        return node.props.title;
+      // current 是指的当前切换的内容
+      const current = computed(() => {
+        return defaults.find(node => {
+          return node.key === props.selected;
+        });
       });
+      const navs = defaults.map(node => {
+        return {
+          title: node.props.title,
+          key: node.props.key
+        };
+      });
+      const select = (key: string) => {
+        context.emit('update:selected', key);
+      };
       return {
         defaults,
-        titles
+        navs,
+        current,
+        select
       };
     }
   };
