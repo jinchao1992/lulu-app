55f251fed27737d5a0d09d4a00696b4a1c65c250
jinchao1992 <jinchaophp@126.com>
2020-12-17 22:52:23 +0800

    :art: 抽离用户需要复制的代码组件

---
+++ src/components/Switch1.demo.vue
@@ -0,0 +1,25 @@
+<template>
+  <Switch v-model:value="bool"/>
+</template>
+
+<script>
+  import Switch from '../lib/Switch.vue'
+  import { ref } from 'vue'
+
+  export default {
+    name: 'Switch1.demo.vue',
+    components: {
+      Switch
+    },
+    setup() {
+      const bool = ref(false)
+      return {
+        bool
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Switch2.demo.vue
@@ -0,0 +1,25 @@
+<template>
+  <Switch v-model:value="bool" disabled/>
+</template>
+
+<script>
+  import Switch from '../lib/Switch.vue'
+  import { ref } from 'vue'
+
+  export default {
+    name: 'Switch2.demo.vue',
+    components: {
+      Switch
+    },
+    setup() {
+      const bool = ref(false)
+      return {
+        bool
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
