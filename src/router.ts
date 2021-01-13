15d8449bc7f417ebe66d908876b0e49e34e8a633
jinchao1992 <jinchaophp@126.com>
2021-01-13 22:10:29 +0800

    :art: 更新路由名称

--- src/router.ts
+++ src/router.ts
@@ -3,7 +3,7 @@ import Home from './views/Home.vue';
 import Doc from './views/Doc.vue';
 import SwitchDemo from './components/Switch/SwitchDemo.vue';
 import ButtonDemo from './components/Button/ButtonDemo.vue';
-import DiglogDemo from './components/Dialog/DialogDemo.vue';
+import DialogDemo from './components/Dialog/DialogDemo.vue';
 import TabsDemo from './components/Tabs/TabsDemo.vue';
 import { h } from 'vue';
 import Markdown from './components/Markdown.vue';
@@ -55,7 +55,7 @@ const router = createRouter({
         },
         {
           path: 'dialog',
-          component: DiglogDemo
+          component: DialogDemo
         },
         {
           path: 'tabs',
