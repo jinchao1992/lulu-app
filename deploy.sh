c68f495b12b055c1f21ba3da74a8804990a75426
jinchao <jinchaophp@126.com>
2021-02-01 23:02:29 +0800

    :package: 更新 deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,9 @@ git commit -m 'update' &&
 git branch -M 'main' &&
 git remote add origin git@github.com:jinchao1992/lulu-ui-website.git &&
 git push -f -u origin main &&
-cd -
 echo https://jinchao1992.github.io/lulu-ui-website/index.html
+git branch -M 'master' &&
+git remote add gitee git@gitee.com:webjinchao/lulu-app-website.git &&
+git push -f -u gitee master &&
+echo https://webjinchao.gitee.io/lulu-app-website/index.html
+cd -
